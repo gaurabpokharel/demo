48ebf47bcc2d469e086f6c4d193361592970af40
gaurabpokharel <gaurabpokharel3@gmail.com>
2020-11-05 12:35:01 +0545

    any changes??

--- src/main/java/com/demo/DemoApplication.java
+++ src/main/java/com/demo/DemoApplication.java
@@ -9,8 +9,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @SpringBootApplication
 public class DemoApplication {
 
-	
-	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
